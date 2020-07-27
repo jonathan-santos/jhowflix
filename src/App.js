6b401c30ca08229d56ffc7c27dfd879df4056c39
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2020-07-27 11:02:15 -0300

    Added the components Carousel, Footer and BannerMain

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react'
 
+import initialData from './data/initial-data.json'
+
 import Menu from './components/menu'
+import BannerMain from './components/bannerMain'
+import Carousel from './components/carousel'
+import Footer from './components/footer'
 
 const App = () => (
   <div className='App'>
     <Menu />
+
+    <BannerMain
+      videoTitle={initialData.categories[0].videos[0].title}
+      url={initialData.categories[0].videos[0].url}
+      videoDescription='What is Front-End?'
+    />
+
+    <Carousel
+      ignoreFirstVideo
+      category={initialData.categories[0]}
+    />
+
+    <Footer />
   </div>
 )
 
